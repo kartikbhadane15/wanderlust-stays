1583f149b49a376dc0d17fb9c5df3861832325af
kartikbhadane15 <kartikbhadane004@gmail.com>
2025-10-02 23:37:19 +0530

    install package passport and other & added router signup get and post

--- app.js
+++ app.js
@@ -7,9 +7,13 @@ const ejsMate = require("ejs-mate");
 const ExpressError = require("./utils/ExpressError.js");
 const session = require("express-session");
 const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user.js");
 
-const listings = require("./routes/listing.js");
-const reviews = require("./routes/review.js");
+const listingRouter = require("./routes/listing.js");
+const reviewRouter = require("./routes/review.js");
+const userRouter = require("./routes/user.js");  
 
 // ----------------------- Database Config -----------------------
 
@@ -55,22 +59,38 @@ app.get("/", (req, res) => {
 app.use(session(sessionOptions));
 app.use(flash());
 
+// ----------------------- Passport Config -----------------------
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+
 app.use((req, res, next) => {
   res.locals.success = req.flash("success");
   res.locals.error = req.flash("error");
   next();
 });
 
-// Using Routes
-app.use("/listings", listings);
-app.use("/listings/:id/reviews", reviews);
+// app.get("/demoUser", async (req, res) => {
+//   let fakeUser = new User({ 
+//     email: "student@gmail.com",
+//     username: "delta-student"
+//     });
 
+//     let registeredUser = await User.register(fakeUser, "helloworld");
+//     res.send(registeredUser);
+//   });
+
+// Using Routes
+app.use("/listings", listingRouter);
+app.use("/listings/:id/reviews", reviewRouter);
+app.use("/", userRouter);
 
 
 // ----------------------- Error Handling -----------------------
-app.all("*", (req, res, next) => {
+app.use((req, res, next) => {
   next(new ExpressError(404, "Page Not Found"));
 });
+
 
 app.use((err, req, res, next) => {
   let { statusCode = 500, message = "Something went wrong" } = err;
---
+++ routes/user.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const wrapAsync = require("../utils/wrapAsync");
+const passport = require("passport");
+
+// Login Routes
+router.get("/login", (req, res) => {
+  res.render("users/login.ejs");
+});
+
+router.get("/signup", (req, res) => {
+  res.render("users/signup.ejs");
+});
+
+router.post("/signup", wrapAsync (async (req, res) => {
+try {
+    let { username,email , password } = req.body;
+    const newUser = new User({ username, email, password });
+    const registeredUser = await User.register(newUser, password); 
+    console.log(registeredUser);
+    req.flash("success", "Welcome to the WanderLust!");
+    res.redirect("/listings");
+} catch (e) {
+    req.flash("error", e.message);
+    res.redirect("/signup");
+}
+}));
+
+router.get("/login", (req, res) => {
+    res.render("users/login.ejs");
+});
+
+router.post("/login", passport.authenticate("local", {
+    failureFlash: true,
+    failureRedirect: "/login",  
+}), (req, res) => {
+    req.flash("success", "Welcome back to WanderLust!");
+    res.redirect(redirectUrl);
+});
+
+module.exports = router;
